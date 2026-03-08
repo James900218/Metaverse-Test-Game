8e991af3ef619cffa47099ccf6fa8f42e26494b2
james robinson <jar09rob@hotmail.co.uk>
2026-03-08 15:41:57 +0000

    colliders and follow camera
    added a follow camera so the main camera can be attached to the player boat without being attached to the rigidbody. This is also better for modularity, additionally I added colliders to all the assets. I have tried to save on performance costs where I can, the island is using a convex mesh collider at the moment as its an abstract shape.

---
+++ Metaverse Test Project/Assets/FollowCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform followTransform;
+    private Camera mainCamera;
+
+    public Vector3 cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainCamera.transform.position = followTransform.position + cameraOffset;
+    }
+}
